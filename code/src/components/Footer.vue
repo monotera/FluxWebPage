10a2b844756939dd663345a7c27634849676cd43
monotera <nelson.a.mosquera@gmail.com>
2020-12-30 18:03:19 -0500

    Testing global scss variables

--- code/src/components/Footer.vue
+++ code/src/components/Footer.vue
@@ -1,20 +1,40 @@
 <template>
   <footer>
-    <h1>@FLUXACADEMY</h1>
-    <p>
-      @FLUXACADEMY Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Praesent a viverra magna. Maecenas vitae est nunc. Phasellus vitae odio
-      posuere, cursus purus id, faucibus metus. Cras posuere porttitor elit.
-      Etiam dignissim, purus nec suscipit   pretium.
-    </p>
-    <h2>CONTÁCTANOS</h2>
-    <p>PHONE: 312323323 <br/> EMAIL:  flux@prueba.com  <br/>WHATSAPP:  Click aquí</p>
+    <div class="footer-leftSection">
+      <h1>@FLUXACADEMY</h1>
+      <p>
+        @FLUXACADEMY Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a
+        viverra magna. Maecenas vitae est nunc. Phasellus vitae odio posuere, cursus purus
+        id, faucibus metus. Cras posuere porttitor elit. Etiam dignissim, purus nec
+        suscipit pretium.
+      </p>
+    </div>
+    <div class="footer-middleSection">
+      <h2>CONTÁCTANOS</h2>
+      <p>
+        PHONE: 312323323 <br />
+        EMAIL: flux@prueba.com <br />WHATSAPP: Click aquí
+      </p>
+    </div>
+    <div class="footer-rightSection">
+      <p>Siguenos</p>
+    </div>
     <p>2020 Flux Academy by Manuel Mosquera y Nelson Mosquera</p>
-
   </footer>
 </template>
 <script>
 export default {};
 </script>
 <style lang="scss" scoped>
+* {
+  text-align: center;
+}
+footer {
+  background: $prueba;
+  color: white;
+  
+  .footer-leftSection{
+    align-items: center;
+  }
+}
 </style>
--- code/src/views/Home.vue
+++ code/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>hello</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe inventore iusto quibusdam perspiciatis officia fugit harum illo dicta nemo quia sequi illum quis, et enim repellendus ipsam sit delectus voluptatem?</p>
   </div>
 </template>
 
