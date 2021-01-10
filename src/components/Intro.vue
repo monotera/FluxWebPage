a37da6070ea8d4573d30bf73fe79faf0b6319299
Manuel <mosquera.manuel2011@gmail.com>
2021-01-10 09:46:57 -0500

    Intro component completed

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -5,21 +5,23 @@
             <p class="section-body">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus sem, a laoreet quam. Vivamus non orci vel eros bibendum gravida nec eget leo. Donec nec euismod nibh. Curabitur lacinia erat non arcu sodales, ut fringilla mauris fringilla. Vivamus neque felis, ornare eget volutpat eu, rhoncus in risus. Morbi vitae augue vitae metus ornare tempus sit amet in nibh. Donec ac lacus leo.
             </p>
-            <iframe width="253.93" height="153" src="https://www.youtube.com/embed/mP4hI9UB0Og" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="556" src="https://www.youtube.com/embed/mP4hI9UB0Og" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </section>
-        <section id="benefits">
-            <h2 class="section-header">Beneficios</h2>
-            <p class="section-body">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus sem, a laoreet quam. Vivamus non orci vel eros bibendum gravida nec eget leo. Donec nec euismod nibh. Curabitur lacinia erat non arcu sodales, ut fringilla mauris fringilla.
-            </p>
-        </section>
-        <section id="classes">
-            <h2 class="section-header">Clases</h2>
-            <p class="section-body">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus sem, a laoreet quam. Vivamus non orci vel eros bibendum gravida nec eget leo. Donec nec euismod nibh. Curabitur lacinia erat non arcu sodales, ut fringilla mauris fring.
-            </p>
-            <button id="button-classes">CONOCE MÁS</button>
-        </section>
+        <div class="aside">
+            <section id="benefits">
+                <h2 class="section-header">Beneficios</h2>
+                <p class="section-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus sem, a laoreet quam. Vivamus non orci vel eros bibendum gravida nec eget leo. Donec nec euismod nibh. Curabitur lacinia erat non arcu sodales, ut fringilla mauris fringilla.
+                </p>
+            </section>
+            <section id="classes">
+                <h2 class="section-header">Clases</h2>
+                <p class="section-body">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet finibus sem, a laoreet quam. Vivamus non orci vel eros bibendum gravida nec eget leo. Donec nec euismod nibh. Curabitur lacinia erat non arcu sodales, ut fringilla mauris fring.
+                </p>
+                <button id="button-classes">CONOCE MÁS</button>
+            </section>
+        </div>
     </div>
 </template>
 
@@ -31,26 +33,89 @@
         flex-direction: column;
         align-items: center;
         color: $main-font-color;
+        @media (min-width: $breakpoint-desktop) {
+            padding: 50px 40px;
+            height: 920px;
+            display: grid;
+            grid-template-columns: 7fr 5fr;
+            grid-template-areas: "main aside";
+            column-gap: 20px;
+        }
     }
     section {
         width: 90%;
         margin: 10px 0;
         padding: 20px;
-        border-radius: 10px;
+        border-radius: 4px;
         background-color: $dark-theme-surface;
         display: flex;
         flex-direction: column;
         align-items: center;
-        box-shadow: 0px 5px 10px 5px rgba(0, 0, 0, 0.8);
+        box-shadow: 0 11px 15px -7px rgba(0, 0, 0, 0.2);
         .section-header {
             margin-bottom: 10px;
             font-family: $section-title-font;
             font-size: 18px;
+            @media (min-width: $breakpoint-desktop) {
+                font-size: 36px;
+                margin-bottom: 65px;
+            }
         }
         .section-body {
             margin: 20px 0;
             font-family: $section-body-font;
             font-size: 12px;
+            line-height: 20px;
+            letter-spacing: 0.5px;
+            max-width: 600px;
+            @media (min-width: $breakpoint-desktop) {
+                font-size: 18px;
+                width: 80%;
+                line-height: 25px;
+            }
+        }
+        iframe {
+            width: 100%;
+            max-width: 600px;
+            height: 153px;
+            margin: 25px 0;
+            @media (min-width: $breakpoint-tablet) {
+                height: 250px;
+            }
+            @media (min-width: $breakpoint-desktop) {
+                width: 80%;
+                height: 335px;
+            }
+        }
+        @media (min-width: $breakpoint-desktop) {
+            width: 100%;
+            margin: 0;
+            height: 100%;
+        }
+    }
+    #intro-parkour {
+        @media (min-width: $breakpoint-desktop) {
+            grid-area: main;
+            .section-body,
+            iframe {
+                max-width: 850px;
+            }
+        }
+    }
+    .aside {
+        display: grid;
+        grid-template-rows: 1fr 1fr;
+        justify-items: center;
+        width: 100%;
+        @media (min-width: $breakpoint-desktop) {
+            height: 100%;
+            grid-area: aside;
+            row-gap: 20px;
+        }
+        .section-header {
+            @media (min-width: $breakpoint-desktop) {
+                margin-bottom: 30px;
+            }
         }
     }
     button {
@@ -65,6 +130,9 @@
         &:hover {
             background-color: $secondary-background-color;
             color: $secondary-font-color;
+        }
+        @media (min-width: $breakpoint-desktop) {
+            font-size: 14px;
         }
     }
 </style>
