a64a2354f83ffc8a3482a2bbc61b0bd2ad1cd901
monotera <nelson.a.mosquera@gmail.com>
2021-01-29 23:16:16 -0500

    Connection test 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,54 +1,56 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
-import Store from '../views/Store.vue'
-import Error from '../views/Error404.vue'
-import StoreIndex from '../views/store/Index.vue'
-import Detail from '../views/store/Detail.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Main from "../views/Main.vue";
+import Store from "../views/Store.vue";
+import Error from "../views/Error404.vue";
+import StoreIndex from "../views/store/Index.vue";
+import Detail from "../views/store/Detail.vue";
 
-const Cart = () => import(/* webpackChunkName: "shopping-cart" */ '../views/store/Cart.vue')
+const Cart = () =>
+  import(/* webpackChunkName: "shopping-cart" */ "../views/store/Cart.vue");
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Main',
-    component: Main
+    path: "/",
+    name: "Main",
+    component: Main,
   },
   {
-    path: '/store',
-    name: 'Store',
+    path: "/store",
+    name: "Store",
     component: Store,
     children: [
       {
-        path: '',
-        name: 'StoreIndex',
+        path: "",
+        name: "StoreIndex",
         component: StoreIndex,
       },
       {
-        path: 'cart',
-        name: 'Cart',
+        path: "cart",
+        name: "Cart",
         component: Cart,
       },
       {
-        path: 'products/:name',
-        name: 'Detail',
+        path: "products/:name",
+        name: "Detail",
         component: Detail,
-      }
+      },
     ],
   },
   {
-    path: '/*',
-    name: 'Error',
-    component: Error
+    path: "/*",
+    name: "Error",
+    component: Error,
   },
-]
+];
 
 const router = new VueRouter({
+  index: "/default.html",
   mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
