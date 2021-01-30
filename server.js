5f2b1f1ec3cd11d081473f58ea66428d658824e7
monotera <nelson.a.mosquera@gmail.com>
2021-01-29 23:07:16 -0500

    Connection test 2

--- server.js
+++ server.js
@@ -1,29 +1,16 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const history = require("connect-history-api-fallback");
-const enforce = require("express-sslify");
-const path = require("path");
+const express = require('express');
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
+const enforce = require('express-sslify');
+const path = require('path');
 
 const app = express();
 
-const staticFileMiddleware = express.static(path.join(__dirname + "/dist"));
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-app.use(staticFileMiddleware);
-app.use(
-  history({
-    disableDotRule: true,
-    verbose: true,
-  })
-);
-app.use(staticFileMiddleware);
+app.use(serveStatic(__dirname + '/dist'));
+app.use(history());
 
-app.get("/", function(req, res) {
-  res.render(path.join(__dirname + "/dist/index.html"));
-});
-
-var server = app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-});
+app.listen(process.env.PORT || 5000);
