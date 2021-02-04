dd0069ce2885782eda24f083b56be5c988bad65a
Manuel <mosquera.manuel2011@gmail.com>
2021-02-04 09:19:35 -0500

    StoreHeader created

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app>
+    <StoreHeader/>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <div class="search">
       <router-view></router-view>
     </div>
@@ -7,7 +14,12 @@
 </template>
 
 <script>
-export default {};
+import StoreHeader from '../components/store/StoreHeader'
+export default {
+  components: {
+    StoreHeader,
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
